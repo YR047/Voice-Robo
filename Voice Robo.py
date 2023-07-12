244d21375aa4e5437ede515d8653c6f7bd5a31f1
Yash Ruhil <74950632+YR047@users.noreply.github.com>
2023-07-13 00:39:54 +0530

    update

--- Voice Robo.py
+++ Voice Robo.py
@@ -8,7 +8,7 @@ try:
         if text == "QUIT":
             break
         tts = gTTS(text=text, lang="en")
-        tts.save("speechwriter.mp3")
-        os.system("speechwriter.mp3")
+        tts.save("welcome.mp3")
+        os.system("welcome.mp3")
 except Exception as e:
     print(e)
